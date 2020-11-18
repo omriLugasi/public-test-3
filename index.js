59c5f75978c20dcfba62b99449cfafbcfa1519b6
omri <omlrc1991@gmail.com>
2020-11-18 21:21:10 +0200

    push resta.json

--- index.js
+++ index.js
@@ -1,13 +1,42 @@
 const express = require('express')
-
+const {httpContract} = require('@resta/resta-sdk')
 const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 
-app.get('', (req, res) => {
-  res.send({ date: new Date() })
+const schema = {
+  schemaId: '1',
+  description: 'basic route',
+  automatic: true
+}
+
+app.get('/dor-adn-rion', httpContract(schema), (req, res) => {
+  res.send({
+    date: new Date()
+  })
+})
+
+const schema1 = {
+  schemaId: '2',
+  description: 'basic route',
+  properties: {
+    query: {
+      type: 'object',
+      properties: {
+        id: {
+          type: 'string'
+        }
+      },
+      required: ['id']
+    }
+  }
+}
+
+
+app.get('/one', httpContract(schema1), (req, res) => {
+  res.send(200)
 })
 
 
